ed17b6daa1d7c6ab7e89a84b833ad1c6255d076d
David Reaves <makemesomewaffles@gmail.com>
2019-06-06 15:33:15 +0000

    Save event information to MongoDB.

--- deckinfo/savedecks.js
+++ deckinfo/savedecks.js
@@ -1,4 +1,7 @@
+var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
 var mtg = require('mtgtop8');
 var cnt = 1;
 mtg.standardEvents(function(err, events) {
@@ -9,11 +12,19 @@ mtg.standardEvents(function(err, events) {
         mtg.event(event.id, function(err, single){
             if (err) return console.error(err);
             //console.log(single.decks);
-            console.log(single.title);
-            for (let decks of single.decks){
-                console.log(cnt + ": " + decks.title + " came in " + decks.result);
-                cnt ++;
-            }
+            //console.log(single.title);
+            
+            MongoClient.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("mydb");
+                dbo.createCollection(single.title, function(err, res) {
+                    if (err) throw err;
+                    dbo.collection(single.title).insertOne(single, function(err, res) {
+                        if (err) throw err;
+                    });
+                db.close();
+              });
+            }); 
         });
     }
 });
